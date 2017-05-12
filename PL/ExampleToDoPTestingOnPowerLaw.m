97b895d126079addfc940f612fd7f1398304c93f
Darrell Tay <tayj0037@e.ntu.edu.sg>
2017-05-12 11:57:34 +0800

    Revert "Parameter Estimation for EXP and PL distributions"
    This reverts commit 805a9fd9a1a2cf5f7e7ebc2c517ade9ac72e536f.

--- PL/ExampleToDoPTestingOnPowerLaw.m
+++
@@ -1,9 +0,0 @@
-%%%%% This code need to work with QuickKSDNPValueCalculator,PowerLawFittingCompareResult,Plot_PL
-%%%%%  The code will take hours or days to finish
-%% Perform P-testing
-% load('PL_Sample.mat');
-% Sample = PL_Sample;
-% [Result,PreResult] = PTesting_PL(Sample,cd);
-%% Plot Result
-load('Result_CSNTest.mat');
-Plot_PL(Result,'PL Sample');
